c5d1b70a28c385e7df97ca19bd4f2f2c41b3d9af
Sem31 <31kamleshprajapat@gmail.com>
2024-12-13 04:40:49 +0530

    Moduling css style

--- src/App.js
+++ src/App.js
@@ -1,43 +1,55 @@
-import { AgeCounter } from "./AgeCounter";
+// import { AgeCounter } from "./AgeCounter";
 
+// export function App() {
+//   return (
+//     // <div>
+//     //   <h1>Welcome to the React world</h1>
+//     //   <Greetings
+//     //     firstName={"Rachel"}
+//     //     lastName={"Green"}
+//     //     age={30}
+//     //     car={{ brand: "Viper", color: "red", speed: 200 }}
+//     //     doSomething={function () {
+//     //       console.log("BBLABAEALZKELAZKE");
+//     //     }}
+//     //   >
+//     //     <img src="https://picsum.photos/200/300" />
+//     //   </Greetings>
+//     // </div>
+//     <div>
+//       {/* <input
+//         type="checkbox"
+//         checked
+//         onChange={function () {
+//           console.log("You clicked");
+//         }}
+//       />
+//       <h1>Welcome to the React world</h1>
+//       <Greetings
+//         firstName="Rachel"
+//         lastName={"Green"}
+//         age={101}
+//         car={{ brand: "Viper", color: "red", speed: 200 }}
+//         doSomething={function () {
+//           console.log("GLAHEA GLAEAZ");
+//         }}
+//       >
+//         <img src="https://picsum/200/300" />
+//       </Greetings> */}
+
+//       <AgeCounter />
+//     </div>
+//   );
+// }
+
+import "./global.css";
+import { Car } from "./Components/Car/Car";
+import { Greetings } from "./Components/Greetings/Greetings";
 export function App() {
   return (
-    // <div>
-    //   <h1>Welcome to the React world</h1>
-    //   <Greetings
-    //     firstName={"Rachel"}
-    //     lastName={"Green"}
-    //     age={30}
-    //     car={{ brand: "Viper", color: "red", speed: 200 }}
-    //     doSomething={function () {
-    //       console.log("BBLABAEALZKELAZKE");
-    //     }}
-    //   >
-    //     <img src="https://picsum.photos/200/300" />
-    //   </Greetings>
-    // </div>
     <div>
-      {/* <input
-        type="checkbox"
-        checked
-        onChange={function () {
-          console.log("You clicked");
-        }}
-      />
-      <h1>Welcome to the React world</h1>
-      <Greetings
-        firstName="Rachel"
-        lastName={"Green"}
-        age={101}
-        car={{ brand: "Viper", color: "red", speed: 200 }}
-        doSomething={function () {
-          console.log("GLAHEA GLAEAZ");
-        }}
-      >
-        <img src="https://picsum/200/300" />
-      </Greetings> */}
-
-      <AgeCounter />
+      <Car />
+      <Greetings />
     </div>
   );
 }
---
+++ src/Components/Car/Car.jsx
@@ -0,0 +1,3 @@
+export function Car(props) {
+    return <p className="box box2">I am the {"<Car/>"}</p>;
+  }
